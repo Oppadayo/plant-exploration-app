1f7e88c879b7185fd40bd0c79afee171e84ed925
Oppadayo <paula.correapcs@gmail.com>
2021-02-04 21:21:19 -0300

    assets/fonts/ 	assets/icons/adaptive-icon.png 	assets/icons/add.png 	assets/icons/drop.png 	assets/icons/favicon.png 	assets/icons/green-heart.png 	assets/icons/heart.png 	assets/icons/icon.png 	assets/icons/red-heart.png 	assets/icons/splash.png 	assets/icons/thermometer.png 	assets/images/ 	constants/ 	navigation/ 	react-native.config.js 	screens/

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 
-export default function App() {
+import { PlantDetail } from './screens'
+
+import Tabs from './navigation/tabs'
+
+
+const Stack = createStackNavigator()
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+        initialRouteName={'Home'}
+      >
+        <Stack.Screen name='Home' component={Tabs} />
+
+        <Stack.Screen name='PlantDetail' component={PlantDetail} options={{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () =>{
+  return <App />
+}
